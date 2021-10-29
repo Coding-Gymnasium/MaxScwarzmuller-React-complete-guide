9d2c5650ad4a5baa68b8bf5e235a2a5538c3e9ec
Nico Rithner <nicorithner@gmail.com>
2021-10-29 08:32:48 -0600

    Lesson 36. Outputting data and working with expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css';
+
 export const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 9, 28);
+  const expenseTitle = 'Car insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <div>
-      <div>October 28th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
