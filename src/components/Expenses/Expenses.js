592de294051e76473fd533bf9d12d421672cec65
Nico Rithner <nicorithner@gmail.com>
2021-11-08 14:57:12 -0700

    Complete assignment 2. Pass props from child to parent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { Card } from '../UI/Card';
 import { ExpenseItem } from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 export const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
   const list = [...props.expenses];
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className={'expenses'}>
+      <ExpensesFilter
+        defaultYear={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {list.map((expense, i) => {
         return (
           <ExpenseItem
